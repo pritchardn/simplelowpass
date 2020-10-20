4660997a4c9fcc7640e1ab686204caee7118f132
pritchardn <21726929@student.uwa.edu.au>
2020-10-20 11:19:02 +0800

    refactoring

--- src/lowpass.py
+++ src/lowpass.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pyfftw
 
 
-def sinc(x: np.float):
+def sinc(x: np.float64):
     if np.isclose(x, 0.0):
         return 1.0
     else:
@@ -11,14 +10,14 @@ def sinc(x: np.float):
 
 
 def gen_sig(freqs, length, sample_rate):
-    x = np.zeros(length, dtype=np.float)
+    x = np.zeros(length, dtype=np.float64)
     for f in freqs:
         for i in range(length):
             x[i] += np.sin(2 * np.pi * i * f / sample_rate)
     return x
 
 
-def window(length, cutoff, sample_rate):
+def gen_window(length, cutoff, sample_rate):
     alpha = 2 * cutoff / sample_rate
     win = np.zeros(length)
     for i in range(length):
@@ -32,33 +31,38 @@ def determine_size(length):
     return int(2 ** np.ceil(np.log2(length)))
 
 
-def fftea_time_np(signal: np.array, window: np.array):
+def fftea_time_np(freqs: list, sig_len: int, win_len: int, cut_freq: int, srate: int):
+    signal = gen_sig(freqs, sig_len, srate)
+    window = gen_window(win_len, cut_freq, srate)
     nfft = determine_size(len(signal) + len(window) - 1)
-    xzp = np.zeros(nfft)
-    hzp = np.zeros(nfft)
-    xzp[0:len(signal)] = signal
-    hzp[0:len(window)] = window
-    X = np.fft.fft(xzp)
-    H = np.fft.fft(hzp)
-    Y = np.multiply(X, H)
-    y = np.fft.ifft(Y)
-    return np.real(y), np.imag(y)
+    sig_zero_pad = np.zeros(nfft)
+    win_zero_pad = np.zeros(nfft)
+    sig_zero_pad[0:len(signal)] = signal
+    win_zero_pad[0:len(window)] = window
+    sig_fft = np.fft.fft(sig_zero_pad)
+    win_fft = np.fft.fft(win_zero_pad)
+    out_fft = np.multiply(sig_fft, win_fft)
+    out = np.fft.ifft(out_fft)
+    return np.real(out), np.imag(out)
 
 
-def fftea_time_fftw(signal: np.array, window: np.array):
+def fftea_time_fftw(freqs: list, sig_len: int, win_len: int, cut_freq: int, srate: int):
+    import pyfftw
+    signal = gen_sig(freqs, sig_len, srate)
+    window = gen_window(win_len, cut_freq, srate)
     nfft = determine_size(len(signal) + len(window) - 1)
-    xzp = pyfftw.empty_aligned(len(signal), dtype='float64')
-    hzp = pyfftw.empty_aligned(len(window), dtype='float64')
-    xzp[0:len(signal)] = signal
-    hzp[0:len(window)] = window
-    X = pyfftw.interfaces.numpy_fft.fft(xzp, n=nfft)
-    H = pyfftw.interfaces.numpy_fft.fft(hzp, n=nfft)
-    Y = np.multiply(X, H)
-    y = pyfftw.interfaces.numpy_fft.ifft(Y, n=nfft)
-    return np.real(y), np.imag(y)
+    sig_zero_pad = pyfftw.empty_aligned(len(signal), dtype='float64')
+    win_zero_pad = pyfftw.empty_aligned(len(window), dtype='float64')
+    sig_zero_pad[0:len(signal)] = signal
+    win_zero_pad[0:len(window)] = window
+    sig_fft = pyfftw.interfaces.numpy_fft.fft(sig_zero_pad, n=nfft)
+    win_fft = pyfftw.interfaces.numpy_fft.fft(win_zero_pad, n=nfft)
+    out_fft = np.multiply(sig_fft, win_fft)
+    out = pyfftw.interfaces.numpy_fft.ifft(out_fft, n=nfft)
+    return np.real(out), np.imag(out)
 
 
-def fftea_time_cuda(signal: np.array, window: np.array):
+def fftea_time_cuda(freqs: list, sig_len: int, win_len: int, cut_freq: int, srate: int):
     import pycuda.autoinit
     import pycuda.gpuarray as gpuarray
     import skcuda.fft as cu_fft
@@ -66,16 +70,18 @@ def fftea_time_cuda(signal: np.array, window: np.array):
     ctx = pycuda.autoinit.make_default_context()
     ctx.pop()
     linalg.init()
+    signal = gen_sig(freqs, sig_len, srate)
+    window = gen_window(win_len, cut_freq, srate)
     nfft = determine_size(len(signal) + len(window) - 1)
     # Move data to GPU
-    xzp = np.zeros(nfft)
-    hzp = np.zeros(nfft)
-    xzp[0:len(signal)] = signal
-    hzp[0:len(window)] = window
-    sig_gpu = gpuarray.empty((nfft,), dtype=np.float64)
-    win_gpu = gpuarray.empty((nfft,), dtype=np.float64)
-    sig_gpu.set(xzp)
-    win_gpu.set(hzp)
+    sig_zero_pad = np.zeros(nfft)
+    win_zero_pad = np.zeros(nfft)
+    sig_zero_pad[0:len(signal)] = signal
+    win_zero_pad[0:len(window)] = window
+    sig_gpu = gpuarray.empty(sig_zero_pad.shape, dtype=np.float64)
+    win_gpu = gpuarray.empty(win_zero_pad.shape, dtype=np.float64)
+    sig_gpu.set(sig_zero_pad)
+    win_gpu.set(win_zero_pad)
 
     # Plan forwards
     sig_fft_gpu = gpuarray.empty(nfft, np.complex128)
@@ -86,16 +92,20 @@ def fftea_time_cuda(signal: np.array, window: np.array):
     cu_fft.fft(win_gpu, win_fft_gpu, win_plan_forward)
 
     # Convolve
-    y_gpu = linalg.multiply(sig_fft_gpu, win_fft_gpu, overwrite=True)
-    linalg.scale(2.0, y_gpu)
+    out_fft = linalg.multiply(sig_fft_gpu, win_fft_gpu, overwrite=True)
+    linalg.scale(2.0, out_fft)
 
     # Plan inverse
-    out_gpu = gpuarray.empty_like(y_gpu)
-    plan_inverse = cu_fft.Plan(y_gpu.shape, np.complex128, np.complex128)
-    cu_fft.ifft(y_gpu, out_gpu, plan_inverse, True)
+    out_gpu = gpuarray.empty_like(out_fft)
+    plan_inverse = cu_fft.Plan(out_fft.shape, np.complex128, np.complex128)
+    cu_fft.ifft(out_fft, out_gpu, plan_inverse, True)
     out_np = np.zeros(len(out_gpu), np.complex128)
     out_gpu.get(out_np)
     return np.real(out_np), np.imag(out_np)
+
+
+def plot():
+    pass
 
 
 if __name__ == "__main__":
@@ -104,18 +114,10 @@ if __name__ == "__main__":
     L = 256  # Filter size
     cutoff_freq = 600
     sampling_rate = 5000
-    np_sig = gen_sig(frequencies, M, sampling_rate)
-    np_h = window(L, cutoff_freq, sampling_rate)
 
-    fftw_sig = gen_sig(frequencies, M, sampling_rate)
-    fftw_h = window(L, cutoff_freq, sampling_rate)
-
-    cuda_sig = gen_sig(frequencies, M, sampling_rate)
-    cuda_h = window(L, cutoff_freq, sampling_rate)
-
-    np_filtered, error = fftea_time_np(np_sig, np_h)
-    fftw_filtered, fftw_error = fftea_time_fftw(fftw_sig, fftw_h)
-    cuda_filtered, cuda_error = fftea_time_cuda(cuda_sig, cuda_h)
+    np_filtered, error = fftea_time_np(frequencies, M, L, cutoff_freq, sampling_rate)
+    fftw_filtered, fftw_error = fftea_time_fftw(frequencies, M, L, cutoff_freq, sampling_rate)
+    cuda_filtered, cuda_error = fftea_time_cuda(frequencies, M, L, cutoff_freq, sampling_rate)
 
     tol = 1e-15
     print(np.allclose(np_filtered, fftw_filtered, rtol=tol))

3bcb555dfac373477d63b95121041a56b7184fff
pritchardn <21726929@student.uwa.edu.au>
2020-10-22 14:23:02 +0800

    fullrun.sh now creates both single and double precision datasets

--- src/fullrun.sh
+++ src/fullrun.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 mkdir -p ../data/
-mkdir -p ../results/config/noisy
-mkdir -p ../results/config/clean
-mkdir -p ../results/raw/noisy
-mkdir -p ../results/raw/clean
+mkdir -p ../results/double/config/noisy
+mkdir -p ../results/double/config/clean
+mkdir -p ../results/double/raw/noisy
+mkdir -p ../results/double/raw/clean
+mkdir -p ../results/single/config/noisy
+mkdir -p ../results/single/config/clean
+mkdir -p ../results/single/raw/noisy
+mkdir -p ../results/single/raw/clean
 echo "Generating Data"
 python3 input_generator.py ../data/ 10
 echo "Processing from Config"
@@ -14,18 +18,20 @@ RAW_FILES="../data/*.npz"
 for f in $CONF_FILES
 do
   echo "Processing $f"
-  python3 lowpass.py 0 "$f" "../results/config/" 1
+  python3 lowpass.py 0 "$f" "../results/single/config/" 0
+  python3 lowpass.py 0 "$f" "../results/double/config/" 1
 done
 
 echo "Processing from direct files"
 for f in $RAW_FILES
 do
   echo "Processing $f"
-  python3 lowpass.py 1 "$f" "../results/raw/"
+  python3 lowpass.py 1 "$f" "../results/single/raw/" 0
+  python3 lowpass.py 1 "$f" "../results/double/raw/" 1
 done
 
 echo "Repetition Analysis"
-python3 ./postProcessing/repetition_test.py "../results/raw/noisy/" "./postProcessing/repeat" "../results/raw/clean/3_numpy_pointwise.out.npy"
+python3 ./postProcessing/repetition_test.py "../results/double/raw/noisy/" "./postProcessing/repeat" "../results/double/raw/clean/3_numpy_pointwise.out.npy"
 
 echo "Reproduction Analysis 1"
-python3 ./postProcessing/reproduce_test_1.py "../results/raw/clean/" "./postProcessing/reproduce1"
+python3 ./postProcessing/reproduce_test_1.py "../results/double/raw/clean/" "./postProcessing/reproduce1"

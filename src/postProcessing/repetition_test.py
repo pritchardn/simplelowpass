dac05f909d9dae4591e835a426c2463f482e3899
pritchardn <21726929@student.uwa.edu.au>
2020-10-22 13:44:55 +0800

    Adjusts repetition tests to not printout. Adjusted results for new input data

--- src/postProcessing/repetition_test.py
+++ src/postProcessing/repetition_test.py
@@ -25,22 +25,19 @@ if __name__ == '__main__':
     ground_truth = normalize_signal(np.load(fpure))
     # Compute the normalized cross-correlation of itself (expecting 1)
     gcorr = correlate(ground_truth, ground_truth)
-    print(gcorr)
     with open(fout + '.csv', 'w', newline='') as csvf:
         fieldnames = ['Method', 'Normalized Cross Correlation (NCC)']
         writer = csv.DictWriter(csvf, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerow({fieldnames[0]: 'ground_truth', fieldnames[1]: gcorr})
+        writer.writerow({fieldnames[0]: 'ground_truth', fieldnames[1]: gcorr[0]})
         for method in methods:
             average = 0.0
             i = 0
             for name in glob.glob(dir_in + '*_' + method + '.out.npy'):
                 # Compute the normalized cross-correlation of a filtered noisy signal with the filtered pure signal.
                 # This gives us a measure of how effective each method is at filtering.
-                print(name)
                 filtered = normalize_signal(np.load(name))
                 corr = correlate(filtered, ground_truth)
-                print(corr)
                 average += corr
                 i += 1
-            writer.writerow({fieldnames[0]: method, fieldnames[1]: average / i})
+            writer.writerow({fieldnames[0]: method, fieldnames[1]: (average / i)[0]})

64dda898d66ca3b06911b8328e42ad8aa85ff121
pritchardn <21726929@student.uwa.edu.au>
2020-12-01 16:15:20 +0800

    Updates replicate.py to conform to pylint standards

--- src/replicate.py
+++ src/replicate.py
@@ -1,17 +1,29 @@
+"""
+Replicates all published reproducibility tests.
+:argv[1]: relative path of the new computed results
+:argv[2]: relative path of the published results
+"""
 import csv
 import glob
 import json
 import os
 import sys
+import platform
+import psutil
+import GPUtil
 
 from merklelib import MerkleTree
 
+from lowpass import main as lowp
 from postProcessing.repetition_test import main as repeat
 from postProcessing.reproduce_test_1 import main as repro1
 from postProcessing.reproduce_test_2 import main as repro2
 
 
 def find_loaded_modules():
+    """
+    :return: A list of all loaded modules
+    """
     loaded_mods = []
     for name, module in sorted(sys.modules.items()):
         if hasattr(module, '__version__'):
@@ -22,9 +34,11 @@ def find_loaded_modules():
 
 
 def system_summary():
-    import psutil
-    import GPUtil
-    import platform
+    """
+    Summarises the system this function is run on.
+    Includes system, cpu, gpu and module details
+    :return: A dictionary of system details
+    """
     merkletree = MerkleTree()
     system_info = {}
     uname = platform.uname()
@@ -54,22 +68,26 @@ def system_summary():
             'memory': gpu.memoryTotal
         }
     system_info['modules'] = find_loaded_modules()
-    merkletree.append([system_info[key] for key in system_info.keys()])
+    merkletree.append([system_info[item] for item in system_info])
     system_info['signature'] = merkletree.merkle_root
     return system_info
 
 
 def make_dirs(base):
+    """
+    Makes directories for all trial runs
+    :param base: The relative base path
+    """
     print(base + 'data/')
     precision = ['double', 'single']
     quality = ['raw', 'config']
     data_type = ['noisy', 'clean']
-    for p in precision:
-        path = base + 'results/' + p + '/'
-        for q in quality:
-            path2 = path + q + '/'
-            for d in data_type:
-                path3 = path2 + d + '/'
+    for prec in precision:
+        path = base + 'results/' + prec + '/'
+        for qual in quality:
+            path2 = path + qual + '/'
+            for d_type in data_type:
+                path3 = path2 + d_type + '/'
                 print(path3)
                 try:
                     os.makedirs(path3)
@@ -78,33 +96,48 @@ def make_dirs(base):
 
 
 def process_conf(loc, results):
-    from lowpass import main as lowp
+    """
+    Processes all data trials specified by provided configuration files.
+    Expects a double || single /config file structure.
+    :param loc: The relative path location
+    :param results: The relative output path location
+    """
     files = glob.glob(loc + '*.in')
-    for f in files:
-        print(f)
-        lowp(f, results + 'single/config/', False, 0)
-        lowp(f, results + 'double/config/', False, 1)
+    for file in files:
+        print(file)
+        lowp(file, results + 'single/config/', False, 0)
+        lowp(file, results + 'double/config/', False, 1)
 
 
 def process_direct(loc, results):
-    from lowpass import main as lowp
+    """
+    Processes all data trials specified by raw data files.
+    :param loc: The relative input path location
+    :param results: The relative output path location
+    """
     files = glob.glob(loc + "*.npz")
-    for f in files:
-        print(f)
-        lowp(f, results + 'single/raw/', True, 0)
-        lowp(f, results + 'double/raw/', True, 1)
+    for file in files:
+        print(file)
+        lowp(file, results + 'single/raw/', True, 0)
+        lowp(file, results + 'double/raw/', True, 1)
 
 
-def compare_files(f1, f2):
+def compare_files(file_1, file_2):
+    """
+    Compares the outputs in two files
+    :param file_1:
+    :param file_2:
+    :return: A boolean true if both files match contents, false otherwise
+    """
     ret = True
-    with open(f1, 'r') as file1:
+    with open(file_1, 'r') as file1:
         lines1 = file1.readlines()
-    with open(f2, 'r') as file2:
+    with open(file_2, 'r') as file2:
         lines2 = file2.readlines()
     if len(lines1) != len(lines2):
         print("Number of lines do not match")
         return False
-    for i in range(len(lines1)):
+    for i in range(min(len(lines1), len(lines2))):
         if lines1[i] != lines2[i]:
             print(lines1[i] + " does not match " + lines2[i])
             ret = False
@@ -112,6 +145,12 @@ def compare_files(f1, f2):
 
 
 def replicate(base_loc, pub_loc):
+    """
+    Runs all published tests and compares them to the orginal published results.
+    :param base_loc: The relative base path for output files
+    :param pub_loc: The relative base path for published files
+    :return: Prints the status of replication attempts
+    """
     scientific_rep = True
     computational_rep = True
     with open(base_loc + 'results/replicate.csv', 'w') as csvf:
@@ -153,6 +192,12 @@ def replicate(base_loc, pub_loc):
 
 
 def main(base_loc, pub_loc):
+    """
+    Sets up and runs all published tests and compares them to the original published results.
+    :param base_loc: The relative base path for output files
+    :param pub_loc: The relative base path for published files
+    :return: Prints status updates
+    """
     print("Creating result files")
     make_dirs(base_loc)
     print("Processing from config")
@@ -174,6 +219,6 @@ def main(base_loc, pub_loc):
 
 
 if __name__ == "__main__":
-    result_loc = sys.argv[1]
-    published_loc = sys.argv[2]
-    main(result_loc, published_loc)
+    RESULT_LOC = sys.argv[1]
+    PUBLISHED_LOC = sys.argv[2]
+    main(RESULT_LOC, PUBLISHED_LOC)

66afb6b3e9ef19c8750a672544d21d1d35e0374c
pritchardn <21726929@student.uwa.edu.au>
2020-12-01 16:32:28 +0800

    Updates utils.py to conform to pylint standards

--- src/postProcessing/utils.py
+++ src/postProcessing/utils.py
@@ -1,12 +1,26 @@
+"""
+A few utility functions for various low-pass filter trials
+"""
 import numpy as np
 
 
-def normalize_signal(a):
-    amean = np.mean(a)
-    astd = np.std(a)
-    a /= astd
-    return a
+def normalize_signal(series):
+    """
+    Normalises the input series
+    :param series: The input series
+    :return: Normalised input series
+    """
+    # amean = np.mean(series)
+    astd = np.std(series)
+    series /= astd
+    return series
 
 
-def correlate(a, b):
-    return np.absolute(np.correlate(a, b, mode='valid') / len(a))
+def correlate(series_a, series_b):
+    """
+    Computes the cross correlation between two series
+    :param series_a:
+    :param series_b:
+    :return: The cross-correlation between the two input series
+    """
+    return np.absolute(np.correlate(series_a, series_b, mode='valid') / len(series_a))

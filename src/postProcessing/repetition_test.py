1ba3d5639d8e86252195bc08a1d62cb6420b8634
pritchardn <21726929@student.uwa.edu.au>
2020-12-01 16:30:28 +0800

    Updates repetition_test.py to conform to pylint standards

--- src/postProcessing/repetition_test.py
+++ src/postProcessing/repetition_test.py
@@ -1,3 +1,7 @@
+"""
+Performs the published repetition test.
+Computes the Normalised Cross-Correlation between each noisy trial and ground truth.
+"""
 import csv
 import glob
 import sys
@@ -5,24 +9,46 @@ import sys
 import numpy as np
 
 
-def normalize_signal(a):
-    amean = np.mean(a)
-    astd = np.std(a)
-    a /= astd
-    return a
+def normalize_signal(series):
+    """
+    Normalises the input series
+    :param series:
+    :return: Normalised input series
+    """
+    # amean = np.mean(a)
+    astd = np.std(series)
+    series /= astd
+    return series
 
 
-def correlate(a, b):
-    return np.absolute(np.correlate(a, b, mode='valid') / len(a))
+def correlate(series_a, series_b):
+    """
+    Computes the correlation between two series
+    :param series_a:
+    :param series_b:
+    :return: The NCC of the two-series
+    """
+    return np.absolute(np.correlate(series_a, series_b, mode='valid') / len(series_a))
 
 
-def find_output_formatter(a):
-    length = int(np.ceil(np.log10(len(a))))
+def find_output_formatter(series):
+    """
+    Finds the number of significant places needed to represent full accuracy.
+    :param series: The input data length
+    :return: An output specifier for use with Python's print function
+    """
+    length = int(np.ceil(np.log10(len(series))))
     return "{:." + str(length) + "f}"
 
 
 def main(dir_in, fout, fpure):
-
+    """
+    Performs the published repetition test.
+    :param dir_in: The relative base path
+    :param fout: The relative output path
+    :param fpure: The relative base path for ground-truth data-file location.
+    :return:
+    """
     methods = ['cufft', 'fftw', 'numpy_fft', 'numpy_pointwise']
     # Load filter of a pure signal
     ground_truth = normalize_signal(np.load(fpure))
@@ -33,22 +59,24 @@ def main(dir_in, fout, fpure):
         fieldnames = ['Method', 'Normalized Cross Correlation (NCC)']
         writer = csv.DictWriter(csvf, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerow({fieldnames[0]: 'ground_truth', fieldnames[1]: out_formatter.format(gcorr[0])})
+        writer.writerow({fieldnames[0]: 'ground_truth',
+                         fieldnames[1]: out_formatter.format(gcorr[0])})
         for method in methods:
             average = 0.0
             i = 0
             for name in glob.glob(dir_in + '*_' + method + '.out.npy'):
-                # Compute the normalized cross-correlation of a filtered noisy signal with the filtered pure signal.
+                # Compute the normalized cross-correlation of a filtered noisy
+                # signal with the filtered pure signal.
                 # This gives us a measure of how effective each method is at filtering.
-                filtered = normalize_signal(np.load(name))
-                corr = correlate(filtered, ground_truth)
+                corr = correlate(normalize_signal(np.load(name)), ground_truth)
                 average += corr
                 i += 1
-            writer.writerow({fieldnames[0]: method, fieldnames[1]: out_formatter.format((average / i)[0])})
+            writer.writerow({fieldnames[0]: method,
+                             fieldnames[1]: out_formatter.format((average / i)[0])})
 
 
 if __name__ == '__main__':
-    directory_in = sys.argv[1]
-    file_out = sys.argv[2]
-    file_pure = sys.argv[3]
-    main(directory_in, file_out, file_pure)
+    DIR_IN = sys.argv[1]
+    FILE_OUT = sys.argv[2]
+    FILE_PURE = sys.argv[3]
+    main(DIR_IN, FILE_OUT, FILE_PURE)
